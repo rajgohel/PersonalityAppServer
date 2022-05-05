d90d5583e9f7cdc2ade4ecafe793700d515b6220
Raj Gohel <rajgohel0001@gmail.com>
2022-05-05 09:19:52 +0530

    constants added

---
+++ constants/apiConstants.js
@@ -0,0 +1,67 @@
+const PORT = 5000;
+const QUESTIONS = [
+    {
+        "category": "Entertainment: Comics",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "You’re really busy at work and a colleague is telling you their life story and personal woes. You:",
+        "correct_answer": "Think it’s more important to give them some of your time; work can wait",
+        "incorrect_answers": [
+            "Don’t dare to interrupt them",
+            "Listen, but with only with half an ear",
+            "Interrupt and explain that you are really busy at the moment"
+        ]
+    },
+    {
+        "category": "Entertainment: Comics",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You:",
+        "correct_answer": "Explain to other equally impatient people in the room that the doctor is always running late",
+        "incorrect_answers": [
+            "Look at your watch every two minutes",
+            "Bubble with inner anger, but keep quiet",
+            "Complain in a loud voice, while tapping your foot impatiently"
+        ]
+    },
+    {
+        "category": "Entertainment: Comics",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You:",
+        "correct_answer": "Think that they are obviously right",
+        "incorrect_answers": [
+            "Don’t dare contradict them",
+            "Defend your own point of view, tooth and nail",
+            "Continuously interrupt your colleague"
+        ]
+    },
+    {
+        "category": "Entertainment: Comics",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "You are taking part in a guided tour of a museum. You:",
+        "correct_answer": "Follow the group without question",
+        "incorrect_answers": [
+            "Are a bit too far towards the back so don’t really hear what the guide is saying",
+            "Make sure that everyone is able to hear properly",
+            "Are right up the front, adding your own comments in a loud voice"
+        ]
+    },
+    {
+        "category": "Entertainment: Comics",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "During dinner parties at your home, you have a hard time with people who:",
+        "correct_answer": "Ask you to tell a story in front of everyone else",
+        "incorrect_answers": [
+            "Talk privately between themselves",
+            "Hang around you all evening",
+            "RachAlways drag the conversation back to themselvesel"
+        ]
+    }
+];
+module.exports = {
+    PORT,
+    QUESTIONS
+}
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { PORT } = require('./constants/apiConstants');
+
 const app = express();
 var cors = require('cors')
-const port = 5000;
+const port = PORT;
+
 var ApiController = require('./controllers/apiController');
 
 app.use(cors());
